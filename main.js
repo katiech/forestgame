071d4242b89dea2a5785b7f1a3349d3a89375777
Emilee Chen <emilee.ty.chen@gmail.com>
2015-12-22 13:40:04 -0800

    cleaned up main.js

--- main.js
+++ main.js
@@ -4,66 +4,45 @@ var thing= 0;
 var rate= 1;
 
 var Auto = setInterval(autoAdd, 1000);
-function autoAdd() {
-   
-   currency = currency + rate;
-   updateAll();
 
+
+function autoAdd() {
+	currency = currency + rate;
+	updateAll();
 }
 
 
 function updateAll(){
-
-document.getElementById("cost").innerHTML = cost;
-document.getElementById("currency").innerHTML = currency;
-document.getElementById("things").innerHTML = thing;
-
+	document.getElementById("cost").innerHTML = cost;
+	document.getElementById("currency").innerHTML = currency;
+	document.getElementById("things").innerHTML = thing;
 }
 
-function Add(){
-	
-currency = currency + 1;
-updateAll();
 
+function Add(){	
+	currency = currency + 1;
+	updateAll();
 }
 
-function Buy(){
-
-if (currency >= cost){
-currency = currency - cost;
-thing = thing + 1;
-cost = cost + 1;
-updateAll();
-updateLog(">>Bought a Thing");
-
+function Buy() {
+	if (currency >= cost){
+		currency = currency - cost;
+		thing = thing + 1;
+		cost = cost + 1;
+		updateAll();
+		updateLog(">>Bought a Thing");
+	}
+	else {
+		updateLog(">>Not enough Currency");
+	}
 }
 
-else
-
-updateLog(">>Not enough Currency");
-
-
-
-
-}
 
 function updateLog(string){
-
-
-var oldlog = document.getElementById("log").innerHTML;
-document.getElementById("log").innerHTML = string + "<br>" + oldlog;
-
-
-
+	var oldlog = document.getElementById("log").innerHTML;
+	document.getElementById("log").innerHTML = string + "<br>" + oldlog;
 }
 
-
-
-function clearLog(){
-
-document.getElementById("log").innerHTML = ">>";
-
-
-
-
+function clearLog() {
+	document.getElementById("log").innerHTML = ">>";
 }

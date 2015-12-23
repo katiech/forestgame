bfc1fa2745d5a38df1cadeda7e55d8fcaf05d0d0
Emilee Chen <emilee.ty.chen@gmail.com>
2015-12-22 21:52:47 -0800

    woo we have started

--- main.js
+++ main.js
@@ -1,41 +1,73 @@
-var currency = 0;
-var cost = 5;
-var thing= 0;
-var rate= 1;
-
-var Auto = setInterval(autoAdd, 1000);
 
 
-function autoAdd() {
-	currency = currency + rate;
-	updateAll();
-}
+
+var seed = 0;
+var seedRate = 1;
+
+function seedCollect(number) {
+	seed = seed + number;
+	updateResources();
+};
 
 
-function updateAll(){
-	document.getElementById("cost").innerHTML = cost;
-	document.getElementById("currency").innerHTML = currency;
-	document.getElementById("things").innerHTML = thing;
-}
+
+var sparrow = 0;
+var sparrowCost = 5;
+var sparrowRate = 0;
+
+function buySparrow() {
+	if (seed >= sparrowCost) {
+		sparrow = sparrow + 1;
+		seed = seed - sparrowCost;
+		updateResources();
+		updateRates();
+		updateCosts();
+		updateLog("You have recruited a sparrow.");
+	} else {
+		updateLog("You don't have enough seeds to entice a sparrow to you!");
+	};
+};
 
 
-function Add(){	
-	currency = currency + 1;
-	updateAll();
-}
 
-function Buy() {
-	if (currency >= cost){
-		currency = currency - cost;
-		thing = thing + 1;
-		cost = cost + 1;
-		updateAll();
-		updateLog(">>Bought a Thing");
-	}
-	else {
-		updateLog(">>Not enough Currency");
-	}
-}
+
+function updateResources() {
+	document.getElementById("seed").innerHTML = addSuffix(seed);
+	document.getElementById("sparrow").innerHTML = addSuffix(sparrow);
+};
+
+function updateRates() {
+	seedRate = sparrow;
+	document.getElementById("seedRate").innerHTML = addSuffix(seedRate);
+};
+
+function updateCosts() {
+	sparrowCost = Math.floor(10 * Math.pow(1.1, sparrow));
+	document.getElementById("sparrowCost").innerHTML = addSuffix(sparrowCost);
+};
+
+
+
+
+function addSuffix(resource) {
+	var suffixes = ["K","M","B","T","Qa","Qt","Sx","Sp","Oc","Dc"];
+	for (var i = suffixes.length - 1; i >= 0; i--) {
+		if (resource >= Math.pow(1000, i + 1)) {
+			return (resource / Math.pow(1000, i + 1)).toFixed(2) + suffixes[i];
+		};
+	};
+	return resource;
+};
+
+
+
+
+
+window.setInterval(function(){
+	seedCollect(seedRate);
+}, 1000);		// fires every 1000ms
+
+
 
 
 function updateLog(string){
@@ -46,3 +78,6 @@ function updateLog(string){
 function clearLog() {
 	document.getElementById("log").innerHTML = ">>";
 }
+
+
+

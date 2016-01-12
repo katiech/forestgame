c4ea3438796fd0644638f0ceff72a93c86c9290e
Emilee Chen <emilee.ty.chen@gmail.com>
2016-01-11 20:41:33 -0800

    popup text css

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function getElapsedTime() {
 	var timeNow = new Date();
 	var timeThen = new Date(stats.startDate);
 	var elapsed = timeNow.getTime() - timeThen.getTime();
+	console.log(elapsed);
 	return secondsToTime(Math.floor(elapsed / 1000));
 }
 
@@ -628,20 +629,22 @@ function zeroArray(len) {
 }
 
 function secondsToTime(seconds) {
-	var hours	= Math.floor(seconds / 3600);
-	var minutes = Math.floor((seconds - (hours * 3600)) / 60);
-	var seconds = seconds - (hours * 3600) - (minutes * 60);
+	var days 	= Math.floor(seconds / 86400);
+	var hours	= Math.floor((seconds - (days * 86400)) / 3600);
+	var minutes = Math.floor((seconds - (days * 86400) - (hours * 3600)) / 60);
+	var seconds = seconds - (days * 86400) - (hours * 3600) - (minutes * 60);
 	var time = "";
-
-	if (hours != 0) {
-		time = hours + ":";
+	if (days != 0) {
+		time = days + ":";
+	} if (hours != 0) {
+		time += hours + ":";
 	} if (minutes != 0 || time !== "") {
 		minutes = (minutes < 10 && time !== "") ? "0" + minutes : String(minutes);
 		time += minutes + ":";
 	} if (time === "") {
 		time = seconds + "s";
     } else {
-		time += (seconds < 10) ? "0"+seconds : String(seconds);
+		time += (seconds < 10) ? "0" + seconds : String(seconds);
 	}
 	return time;
 }
